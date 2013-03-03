8d8bf481f7465bc329a5143044a7dda3adfeba2b
kidmenot <jacobgeo23@gmail.com>
2013-03-03 19:04:58 +0530

    middle of dev for adding events

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 require(['jquery','jquery.eom'], function($) {
 	$(function() {
-        $("#activate").click(function(){
-        	$("#map").eom({});
-        });
+		$("#map").eom('map',{});
+		$("#scale").eom('scale',{});
+		$.eom('add', 'map',[
+			{
+				title: 'test',
+				description: 'test description',
+				lat: 51.5,
+				lng: -0.09,
+				time: 100
+			}
+		]);
     });
 });
